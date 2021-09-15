4a84199e8336ac2a3772ed3af9bcd01e83f3666f
faustin jacquemain <faustin.jacquemain@sspayment.com>
2021-09-15 11:24:18 +0200

    ncurses

--- src/main.cc
+++ src/main.cc
@@ -4,9 +4,15 @@
 
 int		main(int ac, char **av) {
 
+#ifdef SDL_ENGINE
 	SDL_Init(SDL_INIT_TIMER | SDL_INIT_VIDEO);
 	IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG);
-
+#else
+    initscr();			/* Start curses mode 		*/
+	raw();				/* Line buffering disabled	*/
+	keypad(stdscr, TRUE);		/* We get F1, F2 etc..		*/
+	noecho();
+#endif
 //	if (ac == 2 && ::strcmp(av[1], "--space"))
 	{
 		Game		g;
